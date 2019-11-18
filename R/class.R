25a1bc9f829292217f233614687fde192c32099f
Shigeru ONO <elsur_2001@yahoo.co.jp>
2019-11-18 18:39:51 +0900

    20191118

--- R/class.R
+++ R/class.R
@@ -34,15 +34,29 @@ makeSetting <- function(boardsize, piecesize) {
     #'           }
     #'         }
     #'
-    #' @example example/makeSetting.R
-    
+    #' @examples
+    #' # simple 3x3 puzzle
+    #' mysetting <- makeSetting(
+    #'   boardsize = c(3,3),
+    #'   piecesize = list(
+    #'     "A" = c(1,1),
+    #'     "B" = c(1,1),
+    #'     "C" = c(1,1),
+    #'     "D" = c(1,1),
+    #'     "E" = c(1,1),
+    #'     "F" = c(1,1),
+    #'     "G" = c(1,1)
+    #'   )
+    #' )
+    #' mysetting
+
     # trap: boardsize
     stopifnot(is.numeric(boardsize))
     stopifnot(is.atomic(boardsize))
     stopifnot(length(boardsize) == 2)
     stopifnot(boardsize >= 1)
     stopifnot(boardsize <= 9)
-    
+
     # trap: piecesize
     stopifnot(is.list(piecesize))
     stopifnot(names(piecesize) %in% c(LETTERS, letters))
@@ -52,13 +66,13 @@ makeSetting <- function(boardsize, piecesize) {
     stopifnot(sapply(piecesize, function(x) length(x) == 2))
     stopifnot(sapply(piecesize, function(x) x >= 1))
     stopifnot(sapply(piecesize, function(x) x <= 9))
-    
+
     # loop for each piece
     lPieceArea <- lapply(piecesize, function(anPieceSize) {
         # if anPieceSize == c(2,2) then return a matrix 0, 0 0, 1 1, 0 1, 1
         as.matrix(expand.grid(nRow = seq_len(anPieceSize[1]) - 1, nCol = seq_len(anPieceSize[2]) - 1))
     })
-    
+
     # output
     out <- list(boardsize = boardsize, piecearea = lPieceArea)
     class(out) <- append(class(out), "slidepzl_setting")
@@ -82,10 +96,10 @@ makePiece <- function(type, loc) {
     #' # a piece of type 'A' is on the board,
     #' # whose upper left corner is located at (1, 1)
     #' makePiece('A', c(1,1))
-    
+
     # trap: type
     stopifnot(type %in% c(LETTERS, letters))
-    
+
     # trap: loc
     stopifnot(is.atomic(loc))
     stopifnot(length(loc) == 2)
@@ -95,7 +109,7 @@ makePiece <- function(type, loc) {
         stopifnot(tmploc >= 1)
         stopifnot(tmploc <= 9)
     }
-    
+
     # output
     out <- list(type = type, loc = loc)
     class(out) <- append(class(out), "slidepzl_piece")
@@ -107,13 +121,13 @@ as.character.slidepzl_piece <- function(piece) {
     #' Internal. Convert the location of a piece into a string.
     #'
     #' @param piece a object of 'slidepzl_piece' class.
-    #'              Should have no NA.
+    #'              Should have no NA in location.
     #' @return a character string of length 3
-    
+
     # trap: piece
     stopifnot("slidepzl_piece" %in% class(piece))
     stopifnot(!is.na(piece$loc))
-    
+
     # output
     paste0(c(as.character(piece$loc), piece$type), collapse = "")
 }
@@ -136,17 +150,30 @@ makeState <- function(pieces) {
     #'           }
     #'         }
     #'         Validity of the state is not guaranteed.
-    #' @example example/makeState.R
-    
+    #' @examples
+    #' mystate <- makeState(
+    #'   list(
+    #'     makePiece(type = "A", loc = c(1,1)),
+    #'     makePiece(type = "B", loc = c(1,2)),
+    #'     makePiece(type = "C", loc = c(1,3)),
+    #'     makePiece(type = "D", loc = c(2,1)),
+    #'     makePiece(type = "E", loc = c(2,2)),
+    #'     makePiece(type = "F", loc = c(2,3)),
+    #'     makePiece(type = "G", loc = c(3,1)),
+    #'     makePiece(type = "H", loc = c(3,2))
+    #'   )
+    #' )
+    #' mystate
+
     # trap: pieces
     stopifnot(sapply(pieces, function(x) "slidepzl_piece" %in% class(x)))
-    
+
     # piecetype
     asPieceType <- sapply(pieces, function(x) x$type)
-    
+
     # piecelocs
     mnPieceLocs <- matrix(unlist(lapply(pieces, function(x) x$loc)), ncol = 2, byrow = T)
-    
+
     # output
     out <- list(piecetype = asPieceType, piecelocs = mnPieceLocs)
     class(out) <- append(class(out), "slidepzl_state")
@@ -162,21 +189,21 @@ as.matrix.slidepzl_state <- function(state, setting, error_invalid = TRUE) {
     #' @param setting a object of 'slidepzl_setting' class.
     #' @param error_invalid logical. Raise error if the state is invalid?
     #' @return a character matrix.
-    
+
     # trap: state
     stopifnot("slidepzl_state" %in% class(state))
     if (error_invalid) {
         stopifnot(isValidState(state, setting))
     }
-    
+
     # trap: setting
     stopifnot("slidepzl_setting" %in% class(setting))
-    
+
     # loop for each piece
     lOccupied <- lapply(seq_along(state$piecetype), function(nRow) {
         # get relative area
         mnArea <- setting$piecearea[[state$piecetype[nRow]]]
-        
+
         # characters of each cell
         asPiece <- rep(NA, nrow(mnArea))
         # upper left corner
@@ -187,7 +214,7 @@ as.matrix.slidepzl_state <- function(state, setting, error_invalid = TRUE) {
         asPiece[mnArea[, 1] > 0 & mnArea[, 2] == 0] <- "|"
         # other cells
         asPiece[mnArea[, 1] > 0 & mnArea[, 2] > 0] <- "+"
-        
+
         # convert to absolute area
         mnArea[, 1] <- mnArea[, 1] + state$piecelocs[nRow, 1]
         mnArea[, 2] <- mnArea[, 2] + state$piecelocs[nRow, 2]
@@ -196,7 +223,7 @@ as.matrix.slidepzl_state <- function(state, setting, error_invalid = TRUE) {
         return(mnArea)
     })
     mnOccupied <- do.call(rbind, lOccupied)
-    
+
     # output
     out <- matrix(" ", nrow = setting$boardsize[1], ncol = setting$boardsize[2])
     out[mnOccupied] <- rownames(mnOccupied)
@@ -210,12 +237,12 @@ as.character.slidepzl_state <- function(state) {
     #'
     #' @param state an object of 'slidepzl_state' class.
     #' @return a character vector
-    
+
     # trap: state
     stopifnot("slidepzl_state" %in% class(state))
-    
+
     # e.g. state has pieces A at (1,1), B at (2,2) and B at (3,3)
-    
+
     # asPiece: c('11', '22', '33')
     asPiece <- apply(state$piecelocs, 1, function(x) paste0(as.character(x), collapse = ""))
     # c('11A', '22B', '33C')
